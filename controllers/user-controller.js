1080223be04b3469951c1d8962c0975f86b36c57
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-20 07:57:57 -0400

    added CRUD methods for interacting with User Model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,70 @@
 const { User } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => console.log(err));
+    res.status(400).json(err);
+  },
+  // get user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: "There's no user with this id.. Try again!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // create user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // update user by id
+  updateUserById({ params, body }, res) {
+    // new:true = don't send back the original, return ta new version of the updated document
+    User.findOneAndUpdate({ _id: params }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res
+            .status(400)
+            .json({ message: "There's no user with this id.. Try Again!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  // delete user by id
+
+  deleteUserById({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(400).json({ message: "There's no user with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+};
+
+module.exports = userController;
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // app.use(require("./routes"));
+mongoose.connect("mongodb://localhost:27017/ThinkItOutSocial", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("debug", true);
 
 app.listen(PORT, () => {
   console.log(`Connected to local Host ${PORT}`);
